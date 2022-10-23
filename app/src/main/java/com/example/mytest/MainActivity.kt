63838e4919169bdd2ae81770f485f8c248cce7eb
9lg10 <610569787@qq.com>
2022-10-23 12:49:11 +0800

    Initial Commit

--- app/src/main/java/com/example/mytest/MainActivity.kt
+++ app/src/main/java/com/example/mytest/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("show_lg10_git", "onCreate: ")
+        Log.d("show_lg10_gt", "onCreate: ")
     }
 }
